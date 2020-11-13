5449d791ef6f03efe5312c08723b5c75f0821b46
ryujignh <ryujignh@gmail.com>
2020-11-14 08:45:33 +0900

    Initial commit

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(networkManager.posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("Hacker News")
+        }
+        .onAppear(perform: {
+            networkManager.fetchData()
+        })
     }
 }
 
@@ -19,3 +29,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Hello1"),
+//    Post(id: "3", title: "Hello2"),
+//]
---
+++ Hacker News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  Hacker News
+//
+//  Created by Ryuji Ganaha on 2020/11/14.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
