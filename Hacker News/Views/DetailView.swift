b741141d1fc9256f0cecd49c88afb5e1a5eac79b
ryujignh <ryujignh@gmail.com>
2020-11-14 16:44:03 +0900

    Extract WebView as a separate struct

--- Hacker News/Views/DetailView.swift
+++ Hacker News/Views/DetailView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 struct DetailView: View {
     
@@ -20,24 +19,5 @@ struct DetailView: View {
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.google.com")
-    }
-}
-
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WebView.UIViewType {
-        return WKWebView()
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString {
-            if let url = URL(string: safeString) {
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-        }
-        
     }
 }
